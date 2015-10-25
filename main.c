30f686fbb838092be0b765699f130dd52dcfcecd
Gehygd Admin <gehygd@mail.ru>
2015-10-25 12:03:37 -0700

    move yutilib into github repo, clone into lib

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 
 int main() {
     // reset random
-    srand(time(NULL));
+    /*srand(time(NULL));
 
     RadioTide_Map rt_map = RadioTideMap_newMap();
     int x = 40;
@@ -27,7 +27,36 @@ int main() {
 
 
     // RadioTideWeather_releaseWeatherMap(&rt_weather_map);
-    RadioTideMap_releaseMap(&rt_map);
+    RadioTideMap_releaseMap(&rt_map);*/
+
+
+    // directional node testing
+    Node *node = initNode();
+    node->data = "tits";
+    node->append(node, "bollocks");
+    node->append(node, "wank");
+    node->append(node, "flange");
+    node->append(node, "teapot");
+    node->shift(&node);
+    node->pop(&node);
+    node->pop(&node);
+    node->pop(&node);
+    printf("%d\n", node->length(node));
+
+    Node *current = node;
+    while (current != NULL) {
+        printf("this: %s ", current->data);
+        if (current->next != NULL) {
+            printf("next: %s ", current->next->data);
+        }
+        if (current->prev != NULL) {
+            printf("prev: %s ", current->prev->data);
+        }
+        printf("\n");
+        current = current->next;
+    }
+
+
 
     return 0;
 }
--- src/main.h
+++ src/main.h
@@ -2,6 +2,7 @@
 #define RADIOTIDE_MAIN_H
 
 #include <stdbool.h>
+#include "../lib/libyutil/Yutil_Node.h"
 #include "RadioTide_Map.c"
 #include "RadioTide_Tile.c"
 #include "RadioTide_Weather.c"
